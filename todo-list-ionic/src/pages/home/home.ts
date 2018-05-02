d17ce788f82522d01c9394b598cef19d7f3596eb
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-05-02 10:23:28 +0200

    (page homePage) feat: implement creation of todoForm

--- todo-list-ionic/src/pages/home/home.ts
+++ todo-list-ionic/src/pages/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
-import { TodoService } from "../../shared/services/todo.service";
-import { Todo } from "../../models/todo";
+import {Component, OnInit} from '@angular/core';
+import {IonicPage} from 'ionic-angular';
+import {TodoService} from "../../shared/services/todo.service";
+import {Todo} from "../../models/todo";
 import {Observable} from "rxjs/Observable";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @IonicPage()
 @Component({
@@ -11,9 +12,11 @@ import {Observable} from "rxjs/Observable";
 })
 export class HomePage implements OnInit {
 
-  private _todoObservable: Observable <Todo[]>;
+  private _todoObservable: Observable<Todo[]>;
+  private _todoForm: FormGroup;
 
-  constructor( private _todoService: TodoService ) {}
+  constructor(private _todoService: TodoService, private _fb: FormBuilder) {
+  }
 
   /**
    * @name HomePage#ngOnInit
@@ -22,25 +25,47 @@ export class HomePage implements OnInit {
    */
   ngOnInit(): void {
     this._todoObservable = this.getTodos();
+    this.buildForm();
   }
 
+
+  /**
+   * @name HomePage#buildForm
+   * @type { function }
+   * @description build form to post a todo
+   */
+  buildForm(): void {
+    this._todoForm = this._fb.group({
+      todo: ['', Validators.required]
+    });
+  }
 
   /**
    * @name HomePage#getTodos
    * @type { function }
    * @description get a list of todos
    */
-  getTodos(): Observable <Todo[]> {
+  getTodos(): Observable<Todo[]> {
     return this._todoService.getTodos();
   }
 
   /**
    * @name HomePage#todoObservable
    * @type {getter}
-   * @description Getter of private varibale _todoObservable
+   * @description Getter of private variable _todoObservable
    * @returns {Observable<Todo[]>}
    */
   public get todoObservable(): Observable<Todo[]> {
     return this._todoObservable;
+  }
+
+  /**
+   * @name HomePage#todoForm
+   * @type {getter}
+   * @description Getter of private variable _todoForm
+   * @returns {FormGroup}
+   */
+  public get todoForm(): FormGroup {
+    return this._todoForm;
   }
 }
